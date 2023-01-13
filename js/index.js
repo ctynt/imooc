b6eb9bb63d9b7fd5b257cfd06a964dca5a4449f9
tyx <2597704619qq.com>
2023-01-13 11:02:11 +0800

    完成banner轮播功能

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
  * @Author: yxtong
  * @Date: 2023-01-12 11:09:43
  * @LastEditors: yxtong
- * @LastEditTime: 2023-01-12 11:23:48
+ * @LastEditTime: 2023-01-13 01:09:43
  */
 // 定时器实现关键词切换
 
@@ -24,3 +24,64 @@
         input.placeholder = keyWords[i];
     }, 2000);
 }
+
+{
+    // 定义轮播图数组
+    const swiperImgList = [
+    {
+        path: '../images/swiper/swiper-1.jpg',
+        url: 'https://www.imooc.com/',
+        bg: '../images/swiper/bj-1.jpg'
+    },
+
+    {
+        path: '../images/swiper/swiper-2.jpg',
+        url: 'https://www.imooc.com/',
+        bg: '../images/swiper/bj-2.jpg'
+    },
+
+    {
+        path: '../images/swiper/swiper-3.jpg',
+        url: 'https://www.imooc.com/',
+        bg: '../images/swiper/bj-3.jpg'
+    },
+
+    {
+        path: '../images/swiper/swiper-4.jpg',
+        url: 'https://www.imooc.com/',
+        bg: '../images/swiper/bj-4.jpg'
+    }
+    ];
+
+    // 找到 swiper下的a 标签
+    const swiperA = document.querySelector('.swiper a');
+    // 找到最外层的div
+    const banner = document.querySelector('#banner');
+    // 定时定时器
+    let timer = null;
+    // 数组索引
+    let i = 0;
+
+    // 封装第一个切换图片的函数
+    function changeImg(index) {
+        const obj = swiperImgList[index];
+        swiperA.style.backgroundImage = `url(${obj.path})`;
+        swiperA.href = obj.url;
+        banner.style.backgroundImage = `url(${obj.bg})`;
+    }
+
+    // 初始化轮播，显示第一张图
+    changeImg(i);
+
+    // 启动定时器
+    timer = setInterval(() => {
+        i = ++i ===4 ? 0 : i;
+        changeImg(i);
+    }, 3000);
+}
+
+
+
+
+
+

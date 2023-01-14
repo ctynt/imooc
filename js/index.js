7e16630e504c45b8c36bfcbed1b1cd336f0481a0
tyx <2597704619qq.com>
2023-01-14 13:31:00 +0800

    秒杀拼团整体布局

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
  * @Author: yxtong
  * @Date: 2023-01-12 11:09:43
  * @LastEditors: yxtong
- * @LastEditTime: 2023-01-14 01:03:23
+ * @LastEditTime: 2023-01-14 13:22:46
  */
 // 定时器实现关键词切换
 
@@ -28,43 +28,43 @@
 {
     // 定义轮播图数组
     const swiperImgList = [
-    {
-        path: '../images/swiper/swiper-1.jpg',
-        url: 'https://www.imooc.com/',
-        bg: '../images/swiper/bj-4.jpg'
-    },
+        {
+            path: '../images/swiper/swiper-1.jpg',
+            url: 'https://www.imooc.com/',
+            bg: '../images/swiper/bj-4.jpg'
+        },
 
-    {
-        path: '../images/swiper/swiper-2.jpg',
-        url: 'https://www.imooc.com/',
-        bg: '../images/swiper/bj-4.jpg'
-    },
+        {
+            path: '../images/swiper/swiper-2.jpg',
+            url: 'https://www.imooc.com/',
+            bg: '../images/swiper/bj-4.jpg'
+        },
 
-    {
-        path: '../images/swiper/swiper-3.jpg',
-        url: 'https://www.imooc.com/',
-        bg: '../images/swiper/bj-3.jpg'
-    },
+        {
+            path: '../images/swiper/swiper-3.jpg',
+            url: 'https://www.imooc.com/',
+            bg: '../images/swiper/bj-3.jpg'
+        },
 
-    {
-        path: '../images/swiper/swiper-4.jpg',
-        url: 'https://www.imooc.com/',
-        bg: '../images/swiper/bj-3.jpg'
-    },
+        {
+            path: '../images/swiper/swiper-4.jpg',
+            url: 'https://www.imooc.com/',
+            bg: '../images/swiper/bj-3.jpg'
+        },
 
-    {
-        path: '../images/swiper/swiper-5.jpg',
-        url: 'https://www.imooc.com/',
-        bg: '../images/swiper/bj-4.jpg'
-    },
+        {
+            path: '../images/swiper/swiper-5.jpg',
+            url: 'https://www.imooc.com/',
+            bg: '../images/swiper/bj-4.jpg'
+        },
 
-    {
-        path: '../images/swiper/swiper-6.jpg',
-        url: 'https://www.imooc.com/',
-        bg: '../images/swiper/bj-1.jpg'
-    }
+        {
+            path: '../images/swiper/swiper-6.jpg',
+            url: 'https://www.imooc.com/',
+            bg: '../images/swiper/bj-1.jpg'
+        }
     ];
-    
+
     // 找到 swiper下的a 标签
     const swiperA = document.querySelector('.swiper a');
     // 找到最外层的div
@@ -74,7 +74,7 @@
     // 数组索引
     let i = 0;
 
-      // 找到左右切换按钮
+    // 找到左右切换按钮
     const prevArrow = document.querySelector('.prev');
     const nextArrow = document.querySelector('.next');
 
@@ -89,7 +89,7 @@
         swiperA.style.backgroundImage = `url(${obj.path})`;
         swiperA.href = obj.url;
         banner.style.backgroundImage = `url(${obj.bg})`;
-        
+
         currentCircle(index);
     }
 
@@ -98,7 +98,7 @@
 
     // 启动定时器
     timer = setInterval(() => {
-        i = ++i === 4 ? 0 : i;
+        i = ++i === 6 ? 0 : i;
         changeImg(i);
     }, 3000);
 
@@ -106,17 +106,17 @@
     let flag = true;
     prevArrow.onclick = function () {
         // 如果当前不能点击，直接返回
-        if(flag == false) {
+        if (flag == false) {
             return;
         }
 
         flag = false;
-        i = --i == -1 ? 3 : i;
+        i = --i == -1 ? 5 : i;
         changeImg(i);
         // 一秒后，重新允许点击
         setTimeout(() => {
             flag = true;
-        },1000)
+        }, 1000)
     }
 
     nextArrow.onclick = function () {
@@ -125,17 +125,17 @@
         }
 
         flag = false;
-        i = ++i == 4 ? 0 : i;
+        i = ++i == 6 ? 0 : i;
         changeImg(i);
-        setTimeout(() =>{
+        setTimeout(() => {
             flag = true;
-        },1000)
+        }, 1000)
     }
 
     // 选中的圆点，对其进行函数封装
     function currentCircle(index) {
         for (let i = 0; i < list.length; i++) {
-            list[i].className ='';
+            list[i].className = '';
             list[index].className = 'current';
         }
     }
@@ -144,16 +144,16 @@
     for (let i = 0; i < list.length; i++) {
         list[i].onclick = function () {
             // 如果当前不能点击，直接返回
-            if(flag == false) {
+            if (flag == false) {
                 return;
             }
-    
+
             flag = false;
             changeImg(i);
             setTimeout(() => {
                 flag = true;
-            },1000)
-        } 
+            }, 1000)
+        }
     }
 
     // 鼠标悬停到轮播大图，清除定时器
@@ -164,9 +164,9 @@
     // 鼠标离开，重新启动定时器
     swiperA.onmouseleave = function () {
         timer = setInterval(() => {
-        i = ++i == 4 ? 0 : i;
-        changeImg(i);
-        },3000)
+            i = ++i == 6 ? 0 : i;
+            changeImg(i);
+        }, 3000)
     }
 
     prevArrow.onmouseenter = function () {
@@ -182,6 +182,84 @@
     }
 }
 
+// 倒计时
+{
+    // 获取结束时间点的时间戳
+    let endDate = new Date('2023-1-14 23:06:00)');
+    endDate = parseInt(endDate.getTime() / 1000);
+    console.log(endDate);
+
+    let timer = null;
+
+    // 获取页面的时、分、秒标签
+    const hourDom = document.getElementById('hour');
+    const minDom = document.getElementById('min');
+    const secDom = document.getElementById('sec');
+
+    function coundDown() {
+        // 获取此刻当前时间的时间戳
+        let nowDate = new Date();
+        nowDate = parseInt(nowDate.getTime() / 1000);
+
+        // 计算剩余的总秒数
+        let seconds = endDate - nowDate;
+        // 根据总秒数换算
+        if (seconds >= 0) {
+            let hours = parseInt(seconds / 3600);
+            hours = hours > 9 ? hours : '0' + hours;
+            let mins = parseInt(seconds % 3600 / 60);
+            mins = mins > 9 ? mins : '0' + mins;
+            let secs = seconds % 3600 % 60;
+            secs = secs > 9 ? secs : '0' + secs;
+            console.log(hours, mins, secs);
+
+            // 把结果显示到页面
+            hourDom.innerText = hours;
+            minDom.innerText = mins;
+            secDom.innerText = secs;
+
+        } else {
+            // 清空计时器
+            clearInterval(timer);
+            document.querySelector('.countdown p').innerText = '拼团已结束';
+            hourDom.innerText = '00';
+            minDom.innerText = '00';
+            secDom.innerText = '00';
+        }
+    }
+
+    coundDown();
+
+    // 启动定时器
+    timer = setInterval(() => {
+        coundDown();
+    }, 1000);
+}
+
+// 滚动课程
+{
+    const ul = document.querySelector('./sk-list ul');
+
+    let timer = null;
+
+    let leftPx = 0;
+
+    timer = setInterval(() => {
+        leftPx = --leftPx == -1920 ? 0 : leftPx;
+        ul.style.left = leftPx + 'px';
+    }, 10);
+
+    ul.onmouseenter = function () {
+        clearInterval(timer);
+    }
+
+    ul.onmouseleave = function () {
+        timer = setInterval(() => {
+            leftPx = --leftPx == -1920 ? 0 : leftPx;
+            ul.style.left = leftPx + 'px';
+        }, 10)
+    }
+}
 
 
 
